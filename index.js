56db6a2647fb426819621207fde769eda327be83
tonmoy <tonmoy3673@gmail.com>
2022-11-26 04:46:55 +0600

    set admin,seller,buyer

--- index.js
+++ index.js
@@ -60,6 +60,25 @@ async function run(){
             res.send(result);
         })
 
+        app.get('/users/admin/:email',async(req,res)=>{
+            const email=req.params.email;
+            const query={email}
+            const user=await usersCollection.findOne(query);
+            res.send({isAdmin: user?.role==='admin'});
+        })
+        app.get('/users/seller/:email',async(req,res)=>{
+            const email=req.params.email;
+            const query={email}
+            const user=await usersCollection.findOne(query);
+            res.send({isSeller: user?.role==='seller'});
+        })
+        app.get('/users/buyer/:email',async(req,res)=>{
+            const email=req.params.email;
+            const query={email}
+            const user=await usersCollection.findOne(query);
+            res.send({isBuyer: user?.role==='buyer'});
+        })
+        
         // app.get('/jwt', async(req, res)=>{
         //     const email = req.query.email;
         //     const query = { email: email };
